fc2103bd069ab4e7a6f72f737bf5e18cc88cda85
leskevich <leskevichtema@gmail.com>
2022-11-21 16:56:56 +0100

    refactor hw5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -23,7 +23,7 @@ function Pages() {
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
                 <Route path={'/pre-junior'} element={<PreJunior/>} />
                 <Route path={'/junior'} element={<Junior/>} />
-                <Route path={'/junior-plus*'} element={<JuniorPlus/>} />
+                <Route path={'/junior-plus'} element={<JuniorPlus/>} />
 
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
